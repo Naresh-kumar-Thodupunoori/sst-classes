0eb1ff383e2c20a6295169e49856c5a761d2dc2d
Naresh <nareshkumarthodupunoori@gmail.com>
2024-06-26 10:44:44 +0530

    Added Cart Functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { a, b } from './components/Products/Products';
 import Products from './components/Products/Products';
 import { useState } from 'react';
 import CartContext from './context/CartContext';
+import Cart from './components/Cart/cart';
 function App() {
   // state variable
   // inc
@@ -34,9 +35,10 @@ function App() {
 
   console.log(a, b);
   return (
-    <CartContext.Provider value={{ cart, increaseQuantity, decreaseQuantity}}>
+    <CartContext.Provider value={{ cart, increaseQuantity, decreaseQuantity }}>
       <div className="App">
         <Products  />
+        <Cart/>
       </div>
     </CartContext.Provider>
   );
@@ -65,10 +67,10 @@ export default App;
 // way to consume the state
 
 
-//  Provider
+//  Provider -> the global state is provided by provider
 //  Store
-    // State
-    // Reducer
-// Action
-// Dispatcher
-// Selectors
+    // State -> the global state we want to manage
+    // Reducer -> The way to update the state
+// Action -> makes an action
+// Dispatcher -> dispatcher tells the reducer that the action has been done
+// Selectors -> it helps to use the state
